9c0f670cd6b92f7a72aa00d6de1fd55a73a968b6
nauhtXD <thuanvt090899@gmail.com>
2020-12-08 10:55:23 +0700

    7

--- services/post.service.js
+++ services/post.service.js
@@ -1,11 +1,26 @@
 const models = require("../models/index");
 
 const getAllPosts = async () => {
-  return await models.post.findAll();
+  return await models.post.findAll({
+    include: [
+      {
+        model: models.subcategory,
+        attributes: ["name"],
+      },
+    ],
+  });
 };
 
 const getPost = async (input) => {
-  return await models.post.findOne({ where: { id: input } });
+  return await models.post.findOne({
+    include: [
+      {
+        model: models.subcategory,
+        attributes: ["name"],
+      },
+    ],
+    where: { id: input },
+  });
 };
 
 const createPost = async (input) => {
