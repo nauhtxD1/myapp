787de814be9134c74f43e319d4d4b995043c3da9
Nguyen Duc Tai <taind@bell.vnclear>
2021-01-26 10:53:39 +0700

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build --network=host -t nongnghiep .
 docker stop nongnghiep
 docker container rm nongnghiep
+docker build --network=host -t nongnghiep .
 docker run -e NODE_ENV=develop -d -it --rm --net=host --name nongnghiep -p 3010:8080 nongnghiep:latest
